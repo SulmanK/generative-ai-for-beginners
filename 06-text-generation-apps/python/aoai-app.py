294fdeac0883952f4e3532ccbb586aa2e725b080
Sulman Khan <43801845+SulmanK@users.noreply.github.com>
2024-05-08 12:56:37 +0000

    update exercises

--- 06-text-generation-apps/python/aoai-app.py
+++ 06-text-generation-apps/python/aoai-app.py
@@ -8,17 +8,26 @@ dotenv.load_dotenv()
 # configure Azure OpenAI service client 
 client = AzureOpenAI(
   azure_endpoint = os.environ["AZURE_OPENAI_ENDPOINT"], 
-  api_key=os.environ['AZURE_OPENAI_KEY'],  
+  api_key=os.environ['AZURE_OPENAI_API_KEY'],  
   api_version = "2023-10-01-preview"
   )
 
 deployment=os.environ['AZURE_OPENAI_DEPLOYMENT']
 
 # add your completion code
-prompt = "Complete the following: Once upon a time there was a"
+prompt =  """
+    You're an expert on Data Engineering
+    Suggest a beginner lesson for Data Engineering in the following format:
+
+    Format:
+    - concepts
+    - brief explanation of the lesson:
+    - exercise in building a data pipeline diagram 
+    """
+
 messages = [{"role": "user", "content": prompt}]  
 # make completion
-completion = client.chat.completions.create(model=deployment, messages=messages)
+completion = client.chat.completions.create(model=deployment, messages=messages, max_tokens = 500, temperature = 0.5)
 
 # print response
 print(completion.choices[0].message.content)
